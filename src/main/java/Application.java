76e2edc4a45eebfaf71889dec2ef8a45fffa14f3
Pasha <nikolenko.pavel@gmail.com>
2023-02-07 01:34:17 +0300

    Задание 1.

---
+++ src/main/java/Application.java
@@ -0,0 +1,37 @@
+import java.sql.*;
+
+public class Application {
+    public static void main(String[] args) {
+        final String user = "postgres";
+        final String pass = "minuscan";
+        final String url = "jdbc:postgresql://localhost:5432/skypro";
+
+        try (final Connection connection = DriverManager.getConnection(url, user, pass)) {
+
+            // Задание 1.
+            PreparedStatement statement = connection.prepareStatement(
+                    "SELECT first_name, last_name, gender, city_name\n" +
+                            "FROM employees\n" +
+                            "INNER JOIN city\n" +
+                            "ON employees.city_id = city.city_id\n" +
+                            "WHERE id = (?);"
+            );
+
+            statement.setInt(1, 2);
+
+            final ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                String name = "Имя: " + resultSet.getString("first_name");
+                String lastName = "Фамилия: " + resultSet.getString("last_name");
+
+                System.out.println(name);
+                System.out.println(lastName);
+
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
